7c3655f94f1197c83d809e102363f84885b3113e
viktor <viktor-brui@mail.ru>
2020-10-15 20:05:55 +0300

    fix: correcting spaces

--- src/index.js
+++ src/index.js
@@ -3,19 +3,20 @@ module.exports = function toReadable (number) {
     let dg = ['zero','one','two','three','four','five','six','seven','eight','nine'];
     let tn = ['ten','eleven','twelve','thirteen','fourteen','fifteen','sixteen','seventeen','eighteen','nineteen'];
     let tw = ['twenty','thirty','forty','fifty','sixty','seventy','eighty','ninety'];
-console.log(number);
+
     number = number.toString();
     number = number.replace(/[\, ]/g,'');
     if (number != parseFloat(number)) return 'not a number';
-    var x = number.indexOf('.');
+    let x = number.indexOf('.');
     if (x == -1)
         x = number.length;
     if (x > 15)
         return 'too big';
-    var n = number.split(''); 
-    var str = '';
-    var sk = 0;
-    for (var i=0;   i < x;  i++) {
+    let n = number.split(''); 
+    let str = '';
+    let sk = 0;
+    if (number == 0) return 'zero';
+    for (let i=0;   i < x;  i++) {
         if ((x-i)%3==2) { 
             if (n[i] == '1') {
                 str += tn[Number(n[i+1])] + ' ';
@@ -25,7 +26,7 @@ console.log(number);
                 str += tw[n[i]-2] + ' ';
                 sk=1;
             }
-        } else if (n[i]!=0) { // 0235
+        } else if (n[i]!=0) { 
             str += dg[n[i]] +' ';
             if ((x-i)%3==0) str += 'hundred ';
             sk=1;
@@ -43,13 +44,7 @@ console.log(number);
         for (var i=x+1; i<y; i++)
             str += dg[n[i]] +' ';
     }
-    
-    // console.log('Q' + str.replace(/\s+/g,' ') + 'Q');
-    console.log('Q' + str.replace(/\s+/g,' ') + 'Q');
-    
+
     let numString = str.replace(/\s+/g,' ');
-    if (number == 0) return 'zero';
-
     return numString.trimEnd();
-
 };
